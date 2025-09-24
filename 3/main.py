bfb3c45f36858bd23d71b26a7361d1d6763b3f5f
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-24 21:42:41 +0530

    Create main.py

---
+++ 3/main.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+if n > 0:
+
+     for i in range(0,n):
+          print(i*i)
+
+
+----------------------------
+C:\Users\Lenovo\AppData\Local\Programs\Python\Python313\python.exe "C:\Users\Lenovo\Downloads\marketplace-100_Days_of_Code_-_The_Complete_Python_Pro_Bootcamp-9\contents\Day 2\Data Types\solution.py" 
+5
+0
+1
+4
+9
+16
