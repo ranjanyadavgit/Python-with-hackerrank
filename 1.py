46ef9409f3d540da1651bf6926b7e9b2124124f8
ranjanyadavgit <59697752+ranjanyadavgit@users.noreply.github.com>
2025-09-30 13:50:23 +0530

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,14 @@
+print("Welcome to band generator name")
+
+name_of_city=input("Please enter your city")
+
+name_of_pet=input("Please enter your pet name")
+
+print("your city name is " + name_of_city + " and your pet name is " + name_of_pet)
+
+Lenovo@DESKTOP-M5DT73G MINGW64 /c/Python/day-9
+$ python test.py
+Welcome to band generator name
+Please enter your cityjamshedpur
+Please enter your pet nametommy
+your city name is jamshedpur and your pet name is tommy
